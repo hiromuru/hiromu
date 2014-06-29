aad3ecf6fed8dbbabc47afbe0dc42b3061fef033
hiromu <home@suzuki-akira-no-iMac.local>
2014-06-29 23:14:09 +0900

    oooo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Exam.create(:title => '',:discription => '',:c_at => '',:url => '')
